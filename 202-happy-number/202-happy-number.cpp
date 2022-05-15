479d66b00b656070d25edf3ac9744197ca4ff379
MOHIT914908 <mohitgusain230@gmail.com>
2022-05-15 11:00:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (85.47%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getnum(int n){
+         int sum=0;
+     while(n>0)
+     {
+         int rem=n%10;
+         sum+=rem*rem;
+         n=n/10;
+     }
+        return sum;
+    }
+    bool isHappy(int n) {
+        while(n!=1 && n!=4)
+        {
+            n=getnum(n);
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
