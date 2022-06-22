0480c9622279d0de85b33b95b070f54e720a956c
MOHIT914908 <mohitgusain230@gmail.com>
2022-06-22 23:39:42 +0530

    Time: 3 ms (63.08%), Space: 8.7 MB (61.38%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -22,17 +22,5 @@ public:
         return digits;
     }
 };
-       /* int n=nums.size();
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]<9){
-                nums[i]=nums[i]+1;
-                return nums;
-            }
-            else{
-                nums[i]=0;
-            }
-        }
-        nums[0]=1;
-        nums.push_back(0);
-        return nums;*/
+  
 
