3b71c87296cb056542b08f9c5d1a4c5615e0efe7
MOHIT914908 <mohitgusain230@gmail.com>
2022-10-29 13:43:42 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (96.89%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+       int c=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]==' '){
+            c++;
+        }
+        if(c==k){
+            s.erase(s.begin()+i , s.end());
+            return s;
+        }
+    }
+    return s;
+    }
+};
